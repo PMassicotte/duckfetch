c66b3086a8ee5202bbdc72fd253d9cb9d3a46081
Philippe Massicotte <pmassicotte@hotmail.com>
2024-09-25 08:02:40 -0400

    chore(Cargo.toml): bump version from 0.1.0 to 0.1.1 for new features refactor(cli.rs): remove unused Arg import and update install command description refactor(main.rs): simplify install command logic by removing version argument handling

--- src/duckup/cli.rs
+++ src/duckup/cli.rs
@@ -1,11 +1,11 @@
-use clap::{command, Arg, Command};
+use clap::{command, Command};
 
 /// Builds the command-line interface (CLI) for the DuckDB utility.
 ///
 /// This function sets up the CLI with the following subcommands:
 /// - `list`: Lists all available DuckDB versions.
 /// - `check`: Compares the installed version of DuckDB with the latest release.
-/// - `install`: Installs a specific DuckDB version or the latest version if none is provided.
+/// - `install`: Installs a specific version of DuckDB given a proposed list of releases.
 ///
 /// # Returns
 ///
@@ -19,14 +19,6 @@ pub fn build_cli() -> Command {
         )
         .subcommand(
             Command::new("install")
-                .about("Installs a specific DuckDB version or the latest version if none provided")
-                .arg(
-                    Arg::new("version")
-                        .help("The version of DuckDB to install. Should be in the form of vx.y.z.")
-                        .required(false),
-                ),
-        )
-        .subcommand(
-            Command::new("install-from").about("Install DuckDB using a list of available versions"),
+                .about("Installs a specific version of DuckDB given a proposed list of releases"),
         )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use duckup::duckup::{build_cli, check, duckdb_versions, get_latest_release, install_duckdb};
+use duckup::duckup::{build_cli, check, duckdb_versions, install_duckdb};
 
 fn main() -> Result<()> {
     let mut app = build_cli();
@@ -12,14 +12,7 @@ fn main() -> Result<()> {
         Some(("check", _)) => {
             check()?;
         }
-        Some(("install", install_matches)) => {
-            let requested_version = install_matches
-                .get_one::<String>("version")
-                .map_or_else(get_latest_release, |ver| Ok(ver.to_string()))?;
-
-            install_duckdb(requested_version)?;
-        }
-        Some(("install-from", _)) => {
+        Some(("install", _)) => {
             let available_versions = duckdb_versions()?;
             let tag_names = available_versions.releases();
             let selected_tag = inquire::Select::new(
