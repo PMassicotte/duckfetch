f2e1cbf45d24839b38b4a5bf80b21115205cd88f
Philippe Massicotte <pmassicotte@hotmail.com>
2024-09-27 11:54:31 -0400

    test(url.rs): add platform-specific test cases for build function test(url.rs): refactor test cases to use assert_build helper function for better readability and maintainability

--- src/duckfetch/url.rs
+++ src/duckfetch/url.rs
@@ -116,58 +116,73 @@ pub fn build(tag_name: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::build;
 
+    fn assert_build(tag_name: &str, expected: &str) {
+        let result = build(tag_name);
+        assert_eq!(result, expected);
+    }
+
+    #[cfg(target_os = "linux")]
     #[test]
     fn test_stable_linux_amd64() {
-        let expected =
-            "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-linux-amd64.zip";
-        let result = build("v1.1.1");
-        assert_eq!(result, expected);
+        assert_build(
+            "v1.1.1",
+            "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-linux-amd64.zip",
+        );
     }
 
+    #[cfg(target_os = "linux")]
     #[test]
     fn test_nightly_linux_amd64() {
-        let expected = "https://artifacts.duckdb.org/latest/duckdb-binaries-linux.zip";
-        let result = build("Nightly");
-        assert_eq!(result, expected);
+        assert_build(
+            "Nightly",
+            "https://artifacts.duckdb.org/latest/duckdb-binaries-linux.zip",
+        );
     }
 
-    // How could I mock os and platform
-    //
-    // #[test]
-    // fn test_stable_windows_amd64() {
-    //     let tag_name = "v1.1.1";
-    //     let expected = "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-windows-amd64.zip";
-    //     let result = build(tag_name);
-    //     assert_eq!(result, expected);
-    // }
-    //
-    // #[test]
-    // fn test_nightly_windows() {
-    //     let expected = "https://artifacts.duckdb.org/latest/duckdb-binaries-windows.zip";
-    //     let result = build("Nightly");
-    //     assert_eq!(result, expected);
-    // }
-    //
-    // #[test]
-    // fn test_stable_macos() {
-    //     let expected = "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-osx-universal.zip";
-    //     let result = build("v1.1.1");
-    //     assert_eq!(result, expected);
-    // }
-    //
-    // #[test]
-    // fn test_nightly_macos() {
-    //     let expected = "https://artifacts.duckdb.org/latest/duckdb-binaries-osx.zip";
-    //     let result = build("Nightly");
-    //     assert_eq!(result, expected);
-    // }
-    //
-    // #[test]
-    // fn test_nightly_linux_aarch64() {
-    //     let expected = "https://artifacts.duckdb.org/latest/duckdb-binaries-linux-aarch64.zip";
-    //     let result = build("Nightly");
-    //     assert_eq!(result, expected);
-    // }
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn test_stable_windows_amd64() {
+        assert_build(
+            "v1.1.1",
+            "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-windows-amd64.zip",
+        );
+    }
+
+    #[cfg(target_os = "windows")]
+    #[test]
+    fn test_nightly_windows() {
+        assert_build(
+            "Nightly",
+            "https://artifacts.duckdb.org/latest/duckdb-binaries-windows.zip",
+        );
+    }
+
+    #[cfg(target_os = "macos")]
+    #[test]
+    fn test_stable_macos() {
+        assert_build(
+            "v1.1.1",
+            "https://github.com/duckdb/duckdb/releases/download/v1.1.1/duckdb_cli-osx-universal.zip",
+        );
+    }
+
+    #[cfg(target_os = "macos")]
+    #[test]
+    fn test_nightly_macos() {
+        assert_build(
+            "Nightly",
+            "https://artifacts.duckdb.org/latest/duckdb-binaries-osx.zip",
+        );
+    }
+
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    #[test]
+    fn test_nightly_linux_aarch64() {
+        assert_build(
+            "Nightly",
+            "https://artifacts.duckdb.org/latest/duckdb-binaries-linux-aarch64.zip",
+        );
+    }
 }
