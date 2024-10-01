d172eecfb8636e82656170a437320184fc8d1fa9
Philippe Massicotte <pmassicotte@hotmail.com>
2024-10-01 12:23:25 -0400

    docs(artifacts.rs): add documentation comments for structs and methods to improve code readability and maintainability

--- src/duckfetch/artifacts.rs
+++ src/duckfetch/artifacts.rs
@@ -3,17 +3,24 @@ use reqwest::blocking::Client;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize, Clone)]
+/// Represents an artifact with a name and creation date.
 pub struct Artifacts {
     name: String,
     created_at: String,
 }
 
 #[derive(Debug, Deserialize, Clone)]
+/// Represents a response containing a list of artifacts.
 pub struct ArtifactsResponse {
     artifacts: Vec<Artifacts>,
 }
 
 impl ArtifactsResponse {
+    /// Fetches the list of artifacts from the GitHub API.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<ArtifactsResponse>` - A result containing the ArtifactsResponse or an error.
     pub fn new() -> Result<Self> {
         let client = Client::new();
         let url = "https://api.github.com/repos/duckdb/duckdb/actions/artifacts";
@@ -28,11 +35,17 @@ impl ArtifactsResponse {
         Ok(response)
     }
 
+    /// Finds the creation date of the latest nightly artifact with the name containing 'duckdb-binaries-linux'. This is assuming that the first "artifactf" is the latest produced.
+    ///
+    /// # Returns
+    ///
+    /// * `Result<String>` - A result containing the creation date of the latest nightly artifact or an error.
     pub fn latest_nightly_date(&self) -> Result<String> {
         let created_at = self
             .artifacts
             .iter()
-            .find(|artifact| artifact.name.contains("duckdb-binaries-linux"))
+            .find(|artifact| artifact.name.contains("duckdb-binaries-linux")) // The
+            // find() function return the first find
             .context("No artifact found with name containing 'duckdb-binaries-linux'")?
             .created_at
             .clone();
