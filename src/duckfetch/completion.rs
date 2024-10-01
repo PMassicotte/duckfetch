d5b691be6ba2aa75d2f2300c9d60160817e8ad40
Philippe Massicotte <pmassicotte@hotmail.com>
2024-10-01 09:34:27 -0400

    ci(release.yml): remove push and pull_request triggers to streamline workflow docs(completion.rs): add documentation for print_completions and generate_completions functions to improve code readability and maintainability

--- src/duckfetch/completion.rs
+++ src/duckfetch/completion.rs
@@ -5,10 +5,20 @@ use std::io;
 
 use crate::build_cli;
 
+/// Generates shell completion scripts for the specified shell.
+///
+/// # Arguments
+///
+/// * `gen` - The shell completion generator.
+/// * `cmd` - The command for which to generate completions.
 fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
 }
 
+/// Handles the generation of shell completion scripts based on CLI arguments.
+///
+/// This function checks if the "completions" subcommand is called and if a shell is specified.
+/// If so, it generates the appropriate completion script for the specified shell.
 pub fn generate_completions() {
     let matches = build_cli().get_matches();
 
