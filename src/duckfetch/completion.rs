4776ca73537559530c831849c25dbaf26543828d
Philippe Massicotte <pmassicotte@hotmail.com>
2026-05-13 09:22:57 -0400

    chore(Cargo.toml): update edition to 2024 and dependencies to latest versions for improved compatibility and features
    refactor(completion.rs): rename variable 'gen' to 'generator' for clarity and readability

--- src/duckfetch/completion.rs
+++ src/duckfetch/completion.rs
@@ -11,8 +11,13 @@ use crate::build_cli;
 ///
 /// * `gen` - The shell completion generator.
 /// * `cmd` - The command for which to generate completions.
-fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
-    generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
+fn print_completions<G: Generator>(generator: G, cmd: &mut Command) {
+    generate(
+        generator,
+        cmd,
+        cmd.get_name().to_string(),
+        &mut io::stdout(),
+    );
 }
 
 /// Handles the generation of shell completion scripts based on CLI arguments.
